40bb1e5125136f7d1698e74e00a620c411239e9b
Nicholas Hucal <nhucal@mac.com>
2022-03-09 12:53:55 -0700

    added .env and minor formatting to authenticate

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
@@ -5,22 +6,20 @@ const app = express();
 
 const authenticate = (apiKey, res, next) => {
     if (!apiKey) {
-        res.status(401).send({
+        return res.status(401).send({
             message: "Not Authorized"
         })
-    } else {
-        fs.readFile('./data/api_keys.json', (_err, data) => {
-            const existing = JSON.parse(data)
-            const found = existing.find(ex => String(ex) === String(apiKey))
-            if (!found) {    
-                res.status(401).send({
-                    message: "Not Authorized"
-                })
-            } else {
-                next()
-            }
-        })
-    }
+    } 
+    fs.readFile('./data/api_keys.json', (_err, data) => {
+        const existing = JSON.parse(data)
+        const found = existing.find(ex => String(ex) === String(apiKey))
+        if (!found) {    
+            return res.status(401).send({
+                message: "Not Authorized"
+            })
+        } 
+        next()
+    })
 }
 
 //MIDDLEWARE
@@ -28,6 +27,8 @@ app.use(cors());
 app.use(express.static('./public'));
 app.use(express.json());
 app.use((req, res, next) => {
+    // get headers api key like below
+    //console.log(req.get('x-api-key'));
     if (req.url !== '/register') {
         authenticate(req.query.api_key, res, next)
     } else {
@@ -43,6 +44,6 @@ const registerRoute = require('./routes/register');
 app.use('/register', registerRoute);
 
 // LISTEN
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log('BrainFlix Server Running');
 })
